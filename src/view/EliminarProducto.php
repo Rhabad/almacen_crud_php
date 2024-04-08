d310dc4966176ae757d0c6818addd55317b1cd0b
nicolas_p <nicolas.pizarro@fullstackdev.cl>
2024-04-07 21:47:13 -0400

    un boton cochino

--- src/view/EliminarProducto.php
+++ src/view/EliminarProducto.php
@@ -7,8 +7,34 @@ $id = $_GET['id'];
 
 $result = $producto->deleteProduct($id);
 
-if ($result) {
-    echo "Eliminado";
-} else {
-    echo "No Eliminado";
-}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
+    <script src="../../public/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <?php
+    if ($result) {
+        echo "Eliminado";
+    } else {
+        echo "No Eliminado";
+    }
+    ?>
+
+    <div class="col-md-12">
+        <a class="btn btn-secondary" href="../../index.php">pa' tras cochino</a>
+    </div>
+
+</body>
+
+</html>
